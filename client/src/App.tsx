cd3d3a7a16ac36e013929abbb4adfeea4d27891b
lindoro7 <lindoro2012@yandex.ru>
2022-06-19 20:07:18 +0300

    Add Procfile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className='App'>
+      <Header />
+      <Navbar />
       <h1>Beer CMS</h1>
     </div>
   );
