38cb7f8583a29abc80449dc6beefbeef4da1360a
lindoro7 <lindoro2012@yandex.ru>
2022-07-01 18:02:57 +0300

    Changed inde.js

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ process.env.NODE_ENV === "production"
   : app.use(express.static(path.join(__dirname, "client", "public")));
 
 app.use("/api", router);
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 const start = async () => {
   try {
