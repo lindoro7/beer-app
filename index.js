2e90cc7eaf06e666583a603b2658b813e60868f7
lindoro7 <lindoro2012@yandex.ru>
2022-07-01 13:01:30 +0300

    change Procfile

--- index.js
+++ index.js
@@ -25,22 +25,22 @@ app.use("/api", router);
 const start = async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
-    await sequelize.sync({ force: true });
-    await EquipmentType.bulkCreate([
-      { name: "Пеногасители" },
-      { name: "Заборные головки" },
-      { name: "Охладители" },
-      { name: "Редукторы" },
-      { name: "Краны" },
-      { name: "Башни" },
-    ]);
+    // await sequelize.sync();
+    // await sequelize.sync({ force: true });
+    // await EquipmentType.bulkCreate([
+    //   { name: "Пеногасители" },
+    //   { name: "Заборные головки" },
+    //   { name: "Охладители" },
+    //   { name: "Редукторы" },
+    //   { name: "Краны" },
+    //   { name: "Башни" },
+    // ]);
 
-    await Equipment.bulkCreate([
-      { name: "Заборная головка тип A", equipmentTypeId: 2 },
-      { name: "Заборная головка тип G", equipmentTypeId: 2 },
-      { name: "Заборная головка тип S", equipmentTypeId: 2 },
-    ]);
+    // await Equipment.bulkCreate([
+    //   { name: "Заборная головка тип A", equipmentTypeId: 2 },
+    //   { name: "Заборная головка тип G", equipmentTypeId: 2 },
+    //   { name: "Заборная головка тип S", equipmentTypeId: 2 },
+    // ]);
 
     // await User.bulkCreate([
     //   {
