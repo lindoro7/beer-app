c36b7bf43c4d1cad0fdf0bfa44d7dea069bb27d1
lindoro7 <lindoro2012@yandex.ru>
2022-07-01 13:24:17 +0300

    change express.static folder

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-process.env.NODE_ENV === "production"
-  ? app.use(express.static(path.join(__dirname, "client", "build")))
-  : app.use(express.static(path.join(__dirname, "client", "public")));
+// process.env.NODE_ENV === "production"
+//   ? app.use(express.static(path.join(__dirname, "client", "build")))
+//   : app.use(express.static(path.join(__dirname, "client", "public")));
+
+app.use(express.static(path.join(__dirname, "client", "public")));
 app.use("/api", router);
 
 const start = async () => {
