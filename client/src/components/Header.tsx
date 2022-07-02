7632a4994683bccd30b9e67917328fce79bea26f
lindoro7 <lindoro2012@yandex.ru>
2022-07-02 18:41:03 +0300

    Header viewport padding fix

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 function Header() {
   return (
-    <header className='d-block w-screen text-gray-800 px-10 py-5 bg-gray-500'>
+    <header className='d-block w-screen text-gray-800 md:px-10 md:py-5 px-5 py-2 bg-gray-500'>
       <Navbar />
     </header>
   );
