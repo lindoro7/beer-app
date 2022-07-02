db87eed475142f297915db752a77353cec62481b
lindoro7 <lindoro2012@yandex.ru>
2022-07-02 12:09:03 +0300

    Changed index.js

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ process.env.NODE_ENV === "production"
 // });
 app.use("/api", router);
 
-app.get("*", function (err, req, res, next) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// app.get("*", function (err, req, res, next) {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 const start = async () => {
   try {
